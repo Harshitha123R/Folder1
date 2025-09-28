4b7bf3616a7ef4f6f357ebb99540c00b42bd442b
Harshitha123R <harshithaaaaaaa7@gmail.com>
2025-09-28 11:49:03 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,11 @@
+public class Print {
+  public static void main(String[] args) {
+    int age = 18;
+    double salary = 3500000.00;
+    char grade = 'A';
+    System.out.print("Age:"+age+" Salary:"+salary+" Grade:"+grade);
+  }
+}
+
+
+
