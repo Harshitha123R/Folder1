53eedabd1fbcfa159bbc302a2954d6468339f9e1
Harshitha123R <harshithaaaaaaa7@gmail.com>
2025-09-28 12:00:49 +0530

    Converting datatypes of variables

---
+++ Datatype.java
@@ -0,0 +1,9 @@
+public class Datatype {
+  public static void main(String[] args) {
+    int x=10;
+    double y =x;
+    double a = 9.8;
+    int b = (int)a;
+    System.out.println("a:"+a+"b:"+b);
+  }
+}
